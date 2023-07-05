c21615e5e697f43f4d9d27e60df60cc4dd4dd21a
Yong Chee Ho <yongcheeho@gmail.com>
2023-07-05 17:14:44 +0800

    Added calculation record posting to db

---
+++ src/controllers/calculation-record.controllers.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import { Prisma } from '@prisma/client';
+import prisma from '../utils/prisma.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const allCalculationRecords = await prisma.calculationRecord.findMany();
+  res.json(allCalculationRecords);
+});
+
+router.post('/', async (req, res) => {
+  const data = req.body;
+//   const dateString = data.date
+//   const dateValue = new Date(dateString);
+//   const isoDate = dateValue.toISOString()
+//   data.date=isoDate
+  
+//   const validationErrors = validateUser(data);
+
+//   if (Object.keys(validationErrors).length !== 0)
+//     return res.status(400).send({
+//       error: validationErrors,
+//     });
+
+  try {
+    const calculationRecord = await prisma.calculationRecord.create({
+      data,
+    });
+
+    return res.json(calculationRecord);
+  } catch (err) {
+    if (
+      err instanceof Prisma.PrismaClientKnownRequestError &&
+      err.code === 'P2002'
+    ) {
+      const formattedError = {};
+      formattedError[`${err.meta.target[0]}`] = 'already taken';
+
+      return res.status(500).send({
+        error: formattedError,
+      });
+    }
+    throw err;
+  }
+});
+
+router.delete('/', async (req, res) => {
+    const allCalculationRecords = await prisma.incomeInput.deleteMany();
+    res.json(allCalculationRecords);
+  });
+
+export default router;
