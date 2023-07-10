c1462d852b89c56eed6d96a539207eadfdeeb28a
kee-siang <tksiang.ss@example.com>
2023-07-10 17:00:11 +0800

    add router for delete, get all record or specific record

--- src/controllers/calculation-record.controllers.js
+++ src/controllers/calculation-record.controllers.js
@@ -1,16 +1,54 @@
 import express from 'express';
 import { Prisma } from '@prisma/client';
 import prisma from '../utils/prisma.js';
+import auth from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const allCalculationRecords = await prisma.calculationRecord.findMany();
+router.get('/', auth, async (req, res) => {
+  const userId = req.user.payload.id
+  const allCalculationRecords = await prisma.calculationRecord.findMany({
+    where: {
+      userId: Number(userId),
+    },
+    include: {
+      expenseCategories: true,
+    },
+  });
   res.json(allCalculationRecords);
 });
 
-router.post('/', async (req, res) => {
+router.get(`/:id`, async (req, res) => {
+  const { id } = req.params;
+  const record = await prisma.calculationRecord.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      expenseCategories: true,
+    },
+  });
+  res.json(record);
+});
+
+router.delete(`/:id`, async (req, res) => {
+  const { id } = req.params;
+  await prisma.expensesCategoryForCalculationRecord.deleteMany({
+    where: {
+      CalculationRecordId: Number(id),
+    },
+  });
+  const record = await prisma.calculationRecord.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(record);
+});
+
+router.post('/', auth, async (req, res) => {
   const data = req.body;
+  const userId = req.user.payload.id
 //   const dateString = data.date
 //   const dateValue = new Date(dateString);
 //   const isoDate = dateValue.toISOString()
@@ -25,7 +63,17 @@ router.post('/', async (req, res) => {
 
   try {
     const calculationRecord = await prisma.calculationRecord.create({
-      data,
+      data: {
+        ...data,
+        expenseCategories:{
+          create: data.expenseCategories,
+        },
+        user: {
+          connect: {
+                    id : userId
+                },
+        }
+      }
     });
 
     return res.json(calculationRecord);
